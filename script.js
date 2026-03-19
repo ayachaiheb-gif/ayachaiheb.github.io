2dc29f30b52f8622f55e2f06c23cfe91b1b87e2c
ayachaiheb-gif <ayachaiheb@gmail.com>
2026-03-19 17:26:36 +0000

    script.js

--- script.js
+++ script.js
@@ -154,3 +154,12 @@ requestAnimationFrame(animate);
 init();
 animate();
 }
+const sections = document.querySelectorAll("section");
+
+window.addEventListener("scroll", () => {
+sections.forEach(sec => {
+if(sec.getBoundingClientRect().top < window.innerHeight - 100){
+sec.classList.add("show");
+}
+});
+});
